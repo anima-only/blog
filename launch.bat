48c34827ff259c6355e5ad2b01573039e0dfcae2
railgun081031 <railgun081031@gmail.com>
2020-10-14 08:25:26 +0900

    commit

--- launch.bat
+++ launch.bat
@@ -7,3 +7,12 @@ cd ..
 git add .
 git commit -m "commit"
 git push origin master
+hugo
+cd public
+git add .
+git commit -m "commit"
+git push origin main
+cd ..
+git add .
+git commit -m "commit"
+git push origin master
